0aaa0292ef717b9647d64b377f1d70980ab789f2
maas <maasgultekin@gmail.com>
2013-08-12 17:00:39 +0300

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+  def  home
+
+  end
+
+  def  hastanemiz
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metro::Application.config.secret_token = '01370ae4b0476284aba3872f6227aa9f1f1d3c4140651d51c9df61ff2dc66d379414d72700e3b76b4ed87a64935f2c07ab4e551b42b8974136584c29974612ca'
